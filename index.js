75adaba7c75141039abc47aca72afb8c66e191b9
fortex25 <info@fortexedu.com>
2026-06-17 21:59:31 +0530

    Added webhook endpoint

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Fortex Bot is running!');
+    res.send('Fortex Bot is running!');
+});
+
+app.post('/webhook', (req, res) => {
+    console.log('Webhook received!');
+    console.log(req.body);
+
+    res.status(200).send('OK');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
