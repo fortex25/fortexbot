929c1e56762c7d7415faf35c98ee2a067dc29b08
fortex25 <info@fortexedu.com>
2026-06-17 21:22:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Fortex Bot is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
